ad8c8a797d64e10909a0e18877d502c85f509b16
Pedro Avelar <pedro-curiel@hotmail.com>
2021-09-09 08:29:05 -0300

    App06 | Final | Navigator

--- App_06/lib/main.dart
+++ App_06/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -10,47 +10,117 @@ void main(){
       // ROTAS
       //
       initialRoute: 't1',
-      routes:{
-        't1' : (context) => Tela1(),
-        't2' : (context) => Tela2(),
-        't3' : (context) => Tela3(),
-        't4' : (context) => Tela4(),
+      routes: {
+        't1': (context) => Tela1(),
+        't2': (context) => Tela2(),
+        't3': (context) => Tela3(),
+        't4': (context) => Tela4(),
       },
-
     ),
   );
 }
 
 //
+// Dados
+// 
+class Dados{
+  final String titulo;
+  final String mensagem;
+  Dados(this.titulo,this.mensagem);
+}
+
+
+
+//
 // TELA 1
 //
 class Tela1 extends StatefulWidget {
-  const Tela1({ Key? key }) : super(key: key);
+  const Tela1({Key? key}) : super(key: key);
 
   @override
   _Tela1State createState() => _Tela1State();
 }
 
 class _Tela1State extends State<Tela1> {
+
+  var titulo = TextEditingController();
+  var mensagem = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tela 1')),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        title: Text('Tela 1'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
           children: [
+
+            TextField(
+              controller: titulo,
+              decoration: InputDecoration(
+                labelText: 'Título',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            TextField(
+              controller: mensagem,
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              decoration: InputDecoration(
+                labelText: 'Mensagem',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
             OutlinedButton(
-              child: Text('Próxima'),
-              onPressed: (){
-                
-                Navigator.pushNamed(context, 't2');
+              child: Text('Enviar'),
+              onPressed: () {
+
+                  setState(() {
+
+                    var obj = Dados(
+                      titulo.text,
+                      mensagem.text,    
+                    );
+
+                    Navigator.pushNamed(
+                      context, 
+                      't2',
+                      arguments: obj
+                    );
+                    
+                  });
 
               },
             ),
+
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  child: Text('Próxima'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 't2');
+                  },
+                ),
+              ],
+            ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
@@ -59,7 +129,7 @@ class _Tela1State extends State<Tela1> {
 // TELA 2
 //
 class Tela2 extends StatefulWidget {
-  const Tela2({ Key? key }) : super(key: key);
+  const Tela2({Key? key}) : super(key: key);
 
   @override
   _Tela2State createState() => _Tela2State();
@@ -68,10 +138,51 @@ class Tela2 extends StatefulWidget {
 class _Tela2State extends State<Tela2> {
   @override
   Widget build(BuildContext context) {
+    
+    //
+    // RECUPERAR OS DADOS
+    //
+    final Dados obj = ModalRoute.of(context)!.settings.arguments as Dados;
+    
+    
     return Scaffold(
-      appBar: AppBar(title: Text('Tela 2'),),
+      appBar: AppBar(
+        title: Text('Tela 2'),
+      ),
       body: Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Text('Título'),
+            Text(obj.titulo, style: TextStyle(fontSize: 24),),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Mensagem'),
+            Text(obj.mensagem, style: TextStyle(fontSize: 24),),
 
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  child: Text('Anterior'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                OutlinedButton(
+                  child: Text('Próxima'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 't3');
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -81,14 +192,32 @@ class _Tela2State extends State<Tela2> {
 // TELA 3
 //
 class Tela3 extends StatelessWidget {
-  const Tela3({ Key? key }) : super(key: key);
+  const Tela3({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tela 3'),),
-      body: Container(
-
+      appBar: AppBar(
+        title: Text('Tela 3'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              child: Text('Anterior'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            OutlinedButton(
+              child: Text('Próxima'),
+              onPressed: () {
+                Navigator.pushNamed(context, 't4');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -98,14 +227,26 @@ class Tela3 extends StatelessWidget {
 // TELA 4
 //
 class Tela4 extends StatelessWidget {
-  const Tela4({ Key? key }) : super(key: key);
+  const Tela4({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tela 4'),),
-      body: Container(
-
+      appBar: AppBar(
+        title: Text('Tela 4'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              child: Text('Anterior'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
